9a8b347bc16033083db64a2c1c084f7aa6081851
NevermindBob <vladimir.n.popov@gmail.com>
2022-11-29 09:30:10 +0300

    Initial Commit

---
+++ Rom_DZ_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Rom_DZ_1
+//
+//  Created by Vladimir Popov on 29.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
